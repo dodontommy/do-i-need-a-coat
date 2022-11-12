aee3256e5800098726f1dd3dd9b5973a8d22e6fb
Tommy <machine.valley@gmail.com>
2022-11-12 13:49:16 -0600

    TEST

--- deploy.sh
+++ deploy.sh
@@ -10,5 +10,5 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:dodontommy/do-i-need-a-coat.git master:gh-pages
+git push -f git@github.com:dodontommy/do-i-need-a-coat.git main:gh-pages
 cd -
